834d96c2401ef97fa704be33d5bc987b73fa4906
jfarr095 <jfarr095@uottawa.ca>
2019-01-12 14:39:25 -0500

    First Commit

---
+++ MAKE HACK_quick.cmd
@@ -0,0 +1,9 @@
+﻿cd %~dp0
+
+copy TheNamelessHeroes.gba TheNamelessHeroes_EngTrans.gba
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 "-output:%~dp0TheNamelessHeroes_EngTrans.gba" "-input:%~dp0ROM Buildfile.event"
+
+pause
