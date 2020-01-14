686249ca75774a09eedc29431b9d2a9bf0f176e0
jfarr095 <jfarr095@uottawa.ca>
2020-01-14 14:58:07 -0500

    Crash fix, double [X] termination

--- MAKE HACK_quick.cmd
+++ MAKE HACK_quick.cmd
@@ -10,7 +10,8 @@ cd ".."
 
 if exist "%~dp0ups/ups.exe" (
     cd "%~dp0ups"
-    ups diff -b "%~dp0FE8J_clean.gba" -m "%~dp0TheNamelessHeroes_EngTrans.gba" -o "%~dp0TheNamelessHeroes_EngTrans.ups"
+    ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0TheNamelessHeroes_EngTrans.gba" -o "%~dp0ups/TNH_EngTrans/TNH_EngTrans.ups"
+    ups diff -b "%~dp0FE8J_clean.gba" -m "%~dp0TheNamelessHeroes_EngTrans.gba" -o "%~dp0ups/FE8J_TNH_EngTrans.ups"
 )
 
 pause
